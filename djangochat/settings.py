89a0fe06096f724fad9d7534449392f8fce56159
Yi-Chu <83384846+ada-chu@users.noreply.github.com>
2023-03-12 20:00:53 +0800

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'djangochat.wsgi.application'
 #     'default': dj_database_url.config()
 # }
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+# }
 
 DATABASES = {
   'default': {
